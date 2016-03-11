afd73a8dc53cdfba61468e281b18aefd5f204b27
elzzup <guild0105@gmail.com>
2016-03-12 02:51:34 +0900

    Initial Commit

---
+++ MerryBlue/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MerryBlue
+//
+//  Created by Hiroto Takahashi on 2016/03/12.
+//  Copyright © 2016年 Hiroto Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
