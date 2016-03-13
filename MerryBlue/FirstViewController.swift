cd7075dddd05e942ea2aa61cf8af36157bc85f92
elzzup <guild0105@gmail.com>
2016-03-13 16:08:16 +0900

    Remove modal base

--- MerryBlue/FirstViewController.swift
+++ MerryBlue/FirstViewController.swift
@@ -9,49 +9,18 @@
 import UIKit
 import TwitterKit
 
-class FirstViewController: UIViewController, ModalViewControllerDelegate {
+class FirstViewController: UIViewController {
     
     let modalView = AddWatchUserModalViewController()
     let modalTextLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let addWatchBtn:UIButton = UIButton(frame: CGRectMake(0, 0, 300, 50))
-        addWatchBtn.layer.position = CGPoint(x: self.view.frame.width/2, y: 100)
-        addWatchBtn.setTitle("フォロワーから追加", forState: .Normal)
-        addWatchBtn.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        addWatchBtn.layer.cornerRadius = 10
-        addWatchBtn.layer.borderWidth = 1
-        addWatchBtn.addTarget(self, action: "showModal:", forControlEvents:.TouchUpInside)
-        self.view.addSubview(addWatchBtn)
-        
-        self.modalTextLabel.frame = CGRectMake(0, 0, 300, 50)
-        self.modalTextLabel.layer.position = CGPoint(x: self.view.frame.width/2, y: 200)
-        self.modalTextLabel.textAlignment = .Center
-        self.modalTextLabel.text = "The Modal text is ..."
-        self.modalTextLabel.textColor = UIColor.blackColor()
-        self.view.addSubview(modalTextLabel)
-        
-        self.modalView.delegate = self
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func showModal(sender: AnyObject){
-        self.presentViewController(self.modalView, animated: true, completion: nil)
-    }
-    
-    func modalDidFinished(modalText: String){
-        self.modalTextLabel.text = modalText
-        self.modalView.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-
-
 }
 
