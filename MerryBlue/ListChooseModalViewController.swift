a6d974cf6d13639320415697c5290bf0723d7f83
elzzup <guild0105@gmail.com>
2016-03-14 15:30:10 +0900

    Rename and fix sample

---
+++ MerryBlue/ListChooseModalViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddWatchModalViewController.swift
+//  MerryBlue
+//
+//  Created by Hiroto Takahashi on 2016/03/13.
+//  Copyright © 2016年 Hiroto Takahashi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ListChooseModalViewControllerDelegate {
+    func modalDidFinished(modalText: String)
+}
+
+class ListChooseModalViewController: UIViewController {
+    
+    var delegate: ListChooseModalViewControllerDelegate! = nil
+    let text1 = UITextField()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.orangeColor()
+        
+        self.text1.frame = CGRectMake(0, 0, 300, 50)
+        self.text1.layer.position = CGPoint(x: self.view.frame.width/2, y:100)
+        self.text1.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(self.text1)
+        
+        let submitBtn = UIButton(frame: CGRectMake(0, 0, 300, 50))
+        submitBtn.layer.position = CGPoint(x: self.view.frame.width/2, y:200)
+        submitBtn.setTitle("Submit", forState: .Normal)
+        submitBtn.addTarget(self, action: "submit:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(submitBtn)
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func submit(sender: AnyObject) {
+        self.delegate.modalDidFinished(self.text1.text!)
+    }
+}
