1e0f769aee6142babb4374937915aadd42b60cba
elzzup <guild0105@gmail.com>
2016-03-15 18:49:43 +0900

    TimelineView inti

---
+++ MerryBlue/ListTimelineViewController.swift
@@ -0,0 +1,61 @@
+import UIKit
+import TwitterKit
+import FontAwesomeKit
+
+class ListTimelineViewController: TWTRTimelineViewController {
+    
+    var listId: String!
+    convenience init() {
+        guard let listId: String = ConfigManager.getListId() else {
+            self.init()
+            self.openListsChooser()
+            return
+        }
+        let client = TWTRAPIClient()
+        let dataSource = TWTRListTimelineDataSource(listID: listId, APIClient: client)
+        self.init(dataSource: dataSource)
+        self.title = "ListTimeline"
+        self.setNavigationBar()
+        self.listId = listId
+        TwitterManager.getListUsers(listId)
+    }
+    
+    private func setNavigationBar() {
+        let iconImage = FAKIonIcons.iosListIconWithSize(26).imageWithSize(CGSize(width: 26, height: 26))
+        let switchListButton = UIBarButtonItem(image: iconImage, style: .Plain, target: self, action: "onClickSwitchList")
+        
+        self.navigationController?.navigationBar
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        // self.navigationController?.navigationBar.barTintColor = UIColor.blueColor()
+        // self.navigationController?.navigationBar.alpha = 0.1
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationItem
+        self.navigationItem.title = "ListTimeline"
+        self.navigationItem.setRightBarButtonItem(switchListButton, animated: true)
+    }
+    
+    func onClickSwitchList() {
+        self.openListsChooser()
+    }
+    
+    func openListsChooser() {
+        let vc = UINavigationController(rootViewController: ListChooseViewController())
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    override func didMoveToParentViewController(parent: UIViewController?) {
+        super.willMoveToParentViewController(parent)
+        guard let listId: String = ConfigManager.getListId() else {
+            self.openListsChooser()
+            return
+        }
+        if (self.listId == listId) {
+            return
+        }
+        let client = TWTRAPIClient()
+        let dataSource = TWTRListTimelineDataSource(listID: listId, APIClient: client)
+        self.dataSource = dataSource
+        self.refresh()
+        self.listId = listId
+    }
+}
