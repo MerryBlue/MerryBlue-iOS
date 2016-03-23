6778d2413608cee12533fad17ad4a21fd25ba970
elzzup <guild0105@gmail.com>
2016-03-23 17:09:56 +0900

    Fix Home init loop

--- MerryBlue/ListTimelineViewController.swift
+++ MerryBlue/ListTimelineViewController.swift
@@ -7,7 +7,16 @@ class ListTimelineViewController: TWTRTimelineViewController {
     var list: TwitterList!
     convenience init() {
         guard let list: TwitterList = ListService.sharedInstance.selectHomeList() else {
-            self.init()
+            // TWTRUserTimelineDataSource(screenName: nil, userID: TwitterManager.getUserID(), APIClient: TwitterManager.getClient()
+            // self.init(dataSource: )
+            self.init(dataSource: TWTRUserTimelineDataSource(
+                screenName: nil,
+                userID: TwitterManager.getUserID(),
+                APIClient: TwitterManager.getClient(),
+                maxTweetsPerRequest: 50,
+                includeReplies: false,
+                includeRetweets: false
+                ))
             self.openListsChooser()
             return
         }
