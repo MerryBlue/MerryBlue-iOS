fa0890a8b469514fc12d07f39c3ca52de37bf89d
elzup <guild0105@gmail.com>
2016-04-08 12:44:35 +0900

    Doubletap scale gesture

--- MerryBlue/PhotoViewController.swift
+++ MerryBlue/PhotoViewController.swift
@@ -3,17 +3,55 @@ import UIKit
 // プロトコルを追加
 class PhotoViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var viewerImgView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
 
     var viewerImg: UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.scrollView.delegate = self
+        self.scrollView.minimumZoomScale = 1
+        self.scrollView.maximumZoomScale = 8
+        self.scrollView.scrollEnabled = true
+        self.scrollView.showsHorizontalScrollIndicator = true
+        self.scrollView.showsVerticalScrollIndicator = true
+
+        let doubleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(PhotoViewController.doubleTap(_:)))
+        doubleTapGesture.numberOfTapsRequired = 2
+        self.imageView.userInteractionEnabled = true
+        self.imageView.addGestureRecognizer(doubleTapGesture)
+    }
+
+    func doubleTap(gesture: UITapGestureRecognizer) -> Void {
+
+        // print(self.scrollView.zoomScale)
+        if self.scrollView.zoomScale < self.scrollView.maximumZoomScale {
+
+            let newScale: CGFloat = self.scrollView.zoomScale * 3
+            let zoomRect: CGRect = self.zoomRectForScale(newScale, center: gesture.locationInView(gesture.view))
+            self.scrollView.zoomToRect(zoomRect, animated: true)
+
+        } else {
+            self.scrollView.setZoomScale(1.0, animated: true)
+        }
+    }
+
+    func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect: CGRect = CGRect()
+        zoomRect.size.height = self.scrollView.frame.size.height / scale
+        zoomRect.size.width = self.scrollView.frame.size.width / scale
+
+        zoomRect.origin.x = center.x - zoomRect.size.width / 2.0
+        zoomRect.origin.y = center.y - zoomRect.size.height / 2.0
+
+        return zoomRect
     }
 
     override func viewDidAppear(animated: Bool) {
-        self.viewerImgView.contentMode = .ScaleAspectFit
-        self.viewerImgView.image = viewerImg
+        self.imageView.contentMode = .ScaleAspectFit
+        self.imageView.image = viewerImg
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +61,7 @@ class PhotoViewController: UIViewController, UIScrollViewDelegate {
 
     // ピンチイン・ピンチアウト時に呼ばれる
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return viewerImgView
+        return imageView
     }
 
 }
